22eba2d0c88e5b9e66c279ea00a55a4b12a012e2
Prasanna Nadrajan <prasannanadrajan@gmail.com>
2025-04-07 20:03:09 +0530

    Create power_mod.py

---
+++ power_mod.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+num=int(input())
+num2=int(input())
+mod=int(input())
+print(int(math.pow(num,num2)))
+print(pow(num,num2,mod))
