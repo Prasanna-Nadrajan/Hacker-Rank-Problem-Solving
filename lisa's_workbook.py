bb5c95acc0b041543882b4928f4ecce651a3e7b6
Prasanna Nadrajan <prasannanadrajan@gmail.com>
2025-04-19 18:56:41 +0530

    Create lisa's_workbook.py

---
+++ lisa's_workbook.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'workbook' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY arr
+#
+
+def workbook(n, k, arr):
+    count = 0
+    page = 1  # Start at page 1
+    for problems in arr:
+        temp=1
+        for i in range(1,problems+1):
+            if temp>k:
+                page+=1
+                temp-=k
+            if i==page:
+                count+=1
+            temp+=1
+        page+=1
+    return count
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
