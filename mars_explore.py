21dec17ecf0f416c2ef632129df35ef0a75626af
Prasanna Nadrajan <prasannanadrajan@gmail.com>
2025-06-15 06:19:36 +0530

    Create mars_explore.py

---
+++ mars_explore.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    s1='SOS'
+    change=0
+    for i in range(len(s)):
+        if(s1[i%3]!=s[i]):
+            change+=1
+    return change
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
